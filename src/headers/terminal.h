8b2732e81a742d4d76cded7ee537a14666d3dc77
sepgh <sepehrgh01@gmail.com>
2024-03-14 17:33:18 +0330

    feat(terminal): raw mode

---
+++ src/headers/terminal.h
@@ -0,0 +1,5 @@
+/* Header file for src/terminal.c */
+
+void die(const char *s);
+void enableRawMode();
+void disableRawMode();
--- src/pasche.c
+++ src/pasche.c
@@ -1,5 +1,27 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "headers/terminal.h"
+#include <errno.h>
+
 
 int main() {
+    enableRawMode();
+
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 
+            die("read");
+        
+        if (c == 0) continue;
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
+    }
+    
+    disableRawMode();
     return 0;
 }
